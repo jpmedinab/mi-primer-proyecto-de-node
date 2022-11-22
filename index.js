63964c748f15e0cf779de546f4065257cb2c1242
paola <jpmedinab@correo.udistrital.edu.co>
2022-11-22 04:27:16 -0500

    mi primer commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+//este es el mensaje de bienvenida 
+console.log("Esta es la puerta de entrada al proyecto")
+//esta es la parte intermedia
+/*esta es una prueba de comentarios 
+en varios espacios */
+
+//Este es el mensaje de despedida
+console.log("Adiós")
